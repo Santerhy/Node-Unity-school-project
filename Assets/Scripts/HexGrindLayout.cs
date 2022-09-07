e1f9620cba5b358b6d7e6954f90fef6e926f9d54
unknown <santeri.hytonen@gmail.com>
2022-09-07 15:14:16 +0300

    Created HexRenderer and HexGridlayout

---
+++ Assets/Scripts/HexGrindLayout.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrindLayout : MonoBehaviour
+{
+    [Header("Grid Settings")]
+    public Vector2Int gridSize;
+
+    [Header("Tile Settings")]
+    public float outerSize = 2f;
+    public float innerSize = 0f;
+    public float height = -1f;
+    public bool isFlatTopped = true;
+    public Material material;
+
+    public GameObject hr;
+
+    private void OnEnable()
+    {
+        LayoutGrid();
+    }
+
+    private void OnValidate()
+    {
+        if (Application.isPlaying)
+            LayoutGrid();
+    }
+
+    private void LayoutGrid()
+    {
+        for (int y = 0; y < gridSize.y; y++)
+        {
+            for (int x = 0; x < gridSize.x; x++)
+            {
+                GameObject tile = Instantiate(hr, Vector3.zero, Quaternion.identity);
+                tile.transform.position = GetPositionForHexFromCoordinate(new Vector2Int(x, y));
+
+                HexRenderer hexRenderer = tile.GetComponent<HexRenderer>();
+                hexRenderer.isFlatTopped = isFlatTopped;
+                hexRenderer.outerSize = outerSize;
+                hexRenderer.innerSize = innerSize;
+                hexRenderer.height = height;
+                hexRenderer.SetMaterial(material);
+                hexRenderer.DrawMesh();
+
+                tile.transform.SetParent(transform, true);
+            }
+        }
+    }
+
+    public Vector3 GetPositionForHexFromCoordinate(Vector2Int coordinate)
+    {
+        int column = coordinate.x;
+        int row = coordinate.y;
+        float width;
+        float height;
+        float xPosition;
+        float yPosition;
+        bool shouldOffset;
+        float horizontalDistance;
+        float verticalDistance;
+        float offset;
+        float size = outerSize;
+
+        shouldOffset = (column % 2) == 0;
+        width = 2f * size;
+        height = Mathf.Sqrt(3f) * size;
+
+        horizontalDistance = width * (3f / 4f);
+        verticalDistance = height;
+
+        offset = (shouldOffset) ? height / 2 : 0;
+        xPosition = column * horizontalDistance;
+        yPosition = (row * verticalDistance) - offset;
+
+        return new Vector3(xPosition, 0, -yPosition);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/HexRenderer.cs
+++ Assets/Scripts/HexRenderer.cs
@@ -2,15 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct Face
+{
+    public List<Vector3> vertices { get; private set; }
+    public List<int> triangles { get; private set; }
+    public List<Vector2> uvs { get; private set; }
+
+    public Face(List<Vector3> vertices, List<int> triangles, List<Vector2> uvs)
+    {
+        this.vertices = vertices;
+        this.triangles = triangles;
+        this.uvs = uvs;
+    }
+}
+
+[RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
-[RequireComponent (typeof(MeshFilter))]
 public class HexRenderer : MonoBehaviour
 {
-    Mesh mesh;
-    MeshFilter meshFilter;
-    MeshRenderer meshRenderer;
+    private Mesh mesh;
+    private MeshFilter meshFilter;
+    private MeshRenderer meshRenderer;
+
+    private List<Face> faces;
 
     public Material material;
+    public float innerSize;
+    public float outerSize;
+    public float height;
+    public bool isFlatTopped;
 
     private void Awake()
     {
@@ -23,15 +43,81 @@ public class HexRenderer : MonoBehaviour
         meshFilter.mesh = mesh;
         meshRenderer.material = material;
     }
-    // Start is called before the first frame update
-    void Start()
+
+    public void OnEnable()
     {
-        
+        DrawMesh();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    public void SetMaterial(Material m)
     {
-        
+        material = m;
+    }
+
+    public void DrawMesh()
+    {
+        DrawFaces();
+        CombineFaces();
+    }
+
+    private void DrawFaces()
+    {
+        faces = new List<Face>();
+
+        for (int point = 0; point < 6; point++)
+            faces.Add(CreateFace(innerSize, outerSize, height / 2f, height / 2f, point));
+    }
+
+    private Face CreateFace(float innerRad, float outerRad, float heightA, float heightB, int point, bool reverse = false)
+    {
+        Vector3 pointA = GetPoint(innerRad, heightB, point);
+        Vector3 pointB = GetPoint(innerRad, heightB, (point < 5) ? point + 1 : 0);
+        Vector3 pointC = GetPoint(outerRad, heightA, (point < 5) ? point + 1 : 0);
+        Vector3 pointD = GetPoint(outerRad, heightA, point);
+
+        List<Vector3> vertices = new List<Vector3>() { pointA, pointB, pointC, pointD };
+        List<int> triangles = new List<int>() { 0, 1, 2, 2, 3, 0 };
+        List<Vector2> uvs = new List<Vector2>() { new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1) };
+        if (reverse)
+            vertices.Reverse();
+
+        return new Face(vertices, triangles, uvs);
+    }
+
+    protected Vector3 GetPoint(float size, float height, int index)
+    {
+        float angle_deg = isFlatTopped ? 60 * index: 60*index-30;
+        float angle_rad = Mathf.PI / 180f * angle_deg;
+        return new Vector3((size * Mathf.Cos(angle_rad)), height, size * Mathf.Sin(angle_rad));
+    }
+
+    private void CombineFaces()
+    {
+        List<Vector3> vertices = new List<Vector3> ();
+        List<int> tris = new List<int>();
+        List<Vector2> uvs = new List<Vector2> ();
+
+        for (int i = 0; i < faces.Count; i++)
+        {
+            vertices.AddRange(faces[i].vertices);
+            uvs.AddRange(faces[i].uvs);
+
+            int offset = (4 * i);
+            foreach (int triangle in faces[i].triangles)
+            {
+                tris.Add(triangle + offset);
+            }
+        }
+
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = tris.ToArray();
+        mesh.uv = uvs.ToArray();
+        mesh.RecalculateNormals();
+    }
+
+    public Collider[] CheckNearbyTiles()
+    {
+        return Physics.OverlapSphere(transform.position, 2.18f);
     }
 }
